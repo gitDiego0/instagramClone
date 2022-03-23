1f9995b2aa25c6a3274dd20d27d4758b908cde2a
Diego Garcia <diegogarcianavas.4b@gmail.com>
2022-03-23 19:44:09 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import Header from './components/Header/Header'
+
+function App() {
+  return (
+    <div className="App">
+      <main>
+        <Header />
+      </main>
+    </div>
+  )
+}
+
+export default App
