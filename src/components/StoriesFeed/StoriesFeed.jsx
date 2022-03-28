19a762c052e8751fdb9c1186b6519e6785236690
Diego Garcia <diegogarcianavas.4b@gmail.com>
2022-03-28 18:48:26 +0200

    feat(Root): use of mock data to show stories feed

--- src/components/StoriesFeed/StoriesFeed.jsx
+++ src/components/StoriesFeed/StoriesFeed.jsx
@@ -6,45 +6,62 @@ import styles from './styles.module.css'
 import StorieObject from '../StorieObject/StorieObject'
 
 export default function StoriesFeed() {
-  const onSlideChange = () => {
-    return console.log('slide')
-  }
+  const stories = [
+    {
+      imgSrc: 'https://picsum.photos/id/1/200/200',
+      username: 'Juan',
+    },
+    {
+      imgSrc: 'https://picsum.photos/id/2/200/200',
+      username: 'Paco',
+    },
+    {
+      imgSrc: 'https://picsum.photos/id/3/200/200',
+      username: 'Jesus',
+    },
+    {
+      imgSrc: 'https://picsum.photos/id/4/200/200',
+      username: 'Diego',
+    },
+    {
+      imgSrc: 'https://picsum.photos/id/5/200/200',
+      username: 'Pedro',
+    },
+    {
+      imgSrc: 'https://picsum.photos/id/6/200/200',
+      username: 'Hulio',
+    },
+    {
+      imgSrc: 'https://picsum.photos/id/7/200/200',
+      username: 'Carmen',
+    },
+    {
+      imgSrc: 'https://picsum.photos/id/8/200/200',
+      username: 'Susana',
+    },
+    {
+      imgSrc: 'https://picsum.photos/id/9/200/200',
+      username: 'Maria',
+    },
+    {
+      imgSrc: 'https://picsum.photos/id/10/200/200',
+      username: 'Marta',
+    },
+  ]
+
   return (
     <>
       <Swiper
         spaceBetween={0}
         slidesPerView={4}
         className={styles.carousel}
-        onSlideChange={onSlideChange}
         onSwiper={(swiper) => console.log(swiper)}
       >
-        <SwiperSlide>
-          <StorieObject />
-        </SwiperSlide>
-        <SwiperSlide>
-          <StorieObject />
-        </SwiperSlide>
-        <SwiperSlide>
-          <StorieObject />
-        </SwiperSlide>
-        <SwiperSlide>
-          <StorieObject />
-        </SwiperSlide>
-        <SwiperSlide>
-          <StorieObject />
-        </SwiperSlide>
-        <SwiperSlide>
-          <StorieObject />
-        </SwiperSlide>
-        <SwiperSlide>
-          <StorieObject />
-        </SwiperSlide>
-        <SwiperSlide>
-          <StorieObject />
-        </SwiperSlide>
-        <SwiperSlide>
-          <StorieObject />
-        </SwiperSlide>
+        {stories.map((story, index) => (
+          <SwiperSlide key={index}>
+            <StorieObject {...story} />
+          </SwiperSlide>
+        ))}
       </Swiper>
     </>
   )
