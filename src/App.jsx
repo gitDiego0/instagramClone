cff7f7bfe905f25af7347e2c3cd5a7b9e8387bf8
Diego Garcia <diegogarcianavas.4b@gmail.com>
2022-03-28 18:44:46 +0200

    feat(Root): use of Feed component and Footer on app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import './App.css'
+import Feed from './components/Feed/Feed'
+import Footer from './components/Footer/Footer'
 import Header from './components/Header/Header'
+import Publication from './components/Publication/Publication'
 import StoriesFeed from './components/StoriesFeed/StoriesFeed'
 
 function App() {
@@ -7,7 +10,10 @@ function App() {
     <div className="App">
       <Header />
       <StoriesFeed />
-      <main></main>
+      <main>
+        <Feed />
+      </main>
+      <Footer />
     </div>
   )
 }
