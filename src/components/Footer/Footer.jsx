e9e6c75f045842c4c13e7242793add10d22678fc
Diego Garcia <diegogarcianavas.4b@gmail.com>
2022-03-28 18:45:40 +0200

    feat(Root): Footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import HeartIcon from '../Icons/HeartIcon'
+import Home from '../Icons/Home'
+import PublishIcon from '../Icons/PublishIcon'
+import SearchIcon from '../Icons/SearchIcon'
+import styles from './styles.module.css'
+
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.iconContainer}>
+        <a href="/">
+          <div className={styles.iconWrapper}>
+            <Home />
+          </div>
+        </a>
+        <a href="#">
+          <div className={styles.iconWrapper}>
+            <SearchIcon />
+          </div>
+        </a>
+        <a>
+          <div className={styles.iconWrapper}>
+            <PublishIcon />
+          </div>
+        </a>
+        <a>
+          <div className={styles.iconWrapper}>
+            <HeartIcon />
+          </div>
+        </a>
+        <a>
+          <div className={styles.iconWrapper}>
+            <span className={styles.icon}>h</span>
+          </div>
+        </a>
+      </div>
+    </footer>
+  )
+}
