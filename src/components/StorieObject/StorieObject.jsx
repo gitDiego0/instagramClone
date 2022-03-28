53429034aaf97a5d25cf881adcf29776020de6cc
Diego Garcia <diegogarcianavas.4b@gmail.com>
2022-03-28 18:48:00 +0200

    fix(Root): use of variables instead fixed data

--- src/components/StorieObject/StorieObject.jsx
+++ src/components/StorieObject/StorieObject.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 import styles from './styles.module.css'
 
-export default function StorieObject() {
+export default function StorieObject({ imgSrc, username }) {
   return (
     <div className={styles.storieContainer}>
       <button className={styles.storie}>
         <span className={styles.imageContainer}>
-          <img src="https://cdn.pixabay.com/photo/2018/01/21/14/16/woman-3096664_960_720.jpg" />
+          <img src={imgSrc} alt={username} />
         </span>
-        <span className={styles.username}>Username</span>
+        <span className={styles.username}>{username}</span>
       </button>
     </div>
   )
