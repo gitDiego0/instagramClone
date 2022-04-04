c3ef9ee19d2e103b391bd002978fad13c71cfb9f
Diego Garcia <diegogarcianavas.4b@gmail.com>
2022-04-04 17:14:11 +0200

    removed unused import

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import './App.css'
+
 import Feed from './components/Feed/Feed'
 import Footer from './components/Footer/Footer'
 import Header from './components/Header/Header'
-import Publication from './components/Publication/Publication'
 import StoriesFeed from './components/StoriesFeed/StoriesFeed'
 
 function App() {
